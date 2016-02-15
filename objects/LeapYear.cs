d17a8429d7b58b278a7bbd91fcf46dedba6d6ab1
Nicholas and Will <student@epicodus.com>
2016-02-15 08:30:37 -0800

    Finish project.

--- objects/LeapYear.cs
+++ objects/LeapYear.cs
@@ -11,7 +11,7 @@ namespace LeapYearNS.Objects
 
     public bool IsLeapYear(int year)
     {
-      return year % 4 == 0 && !(year % 100 == 0);
+      return year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0;
     }
   }
 }
